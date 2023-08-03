baf5de1f4e76cb43ee49a22d5fad22ec2898cf37
Jordan Mata <jordanmata15@yahoo.com>
2023-08-02 21:09:44 -0700

    Add multiple color options and eraser

--- etchASketch.js
+++ etchASketch.js
@@ -1,11 +1,17 @@
 const COLOR_SPACE_SIZE = 256;
+const COLORING_MODE = {
+    BLACK: 0,
+    RAINBOW: 1,
+    ERASER: 2 
+}
 
 const boardDiv = document.querySelector(".board");
+const blackButton = document.querySelector("button.black");
+const rainbowButton = document.querySelector("button.rainbow");
+const eraserButton = document.querySelector("button.eraser");
 
+let coloringValue = COLORING_MODE.BLACK;
 let mousePressed = false;
-document.body.onmousedown = () => (mousePressed = true)
-document.body.onmouseup = () => (mousePressed = false)
-
 
 function getTileWidth(tilesPerRow) {
     return Math.floor(boardDiv.clientWidth / tilesPerRow);
@@ -14,7 +20,17 @@ function getTileWidth(tilesPerRow) {
 function assignNewColor(event) {
     let tile = event.target;
     if (event.type === "mouseover" && !mousePressed) return;
-    tile.style["background-color"] = "black";
+
+    let newColor = "black";
+    if (coloringValue === COLORING_MODE.RAINBOW) {
+        let r = Math.floor(Math.random() * COLOR_SPACE_SIZE),
+            g = Math.floor(Math.random() * COLOR_SPACE_SIZE),
+            b = Math.floor(Math.random() * COLOR_SPACE_SIZE);
+        newColor = `rgb(${r},${g},${b})`;
+    } else if (coloringValue === COLORING_MODE.ERASER) {
+        newColor = "white";
+    }
+    tile.style["background-color"] = newColor;
 }
 
 function generateTile(tilesPerRow) {
@@ -29,18 +45,31 @@ function generateTile(tilesPerRow) {
 }
 
 function createGrid(tilesPerRow) {
-    let rowI;
-    let colJ;
     for (let i = 0; i < tilesPerRow; ++i) {
-        rowI = document.createElement('div');
+        let rowI = document.createElement('div');
         rowI.classList.add("row");
         for (let j = 0; j < tilesPerRow; ++j) {
-            colJ = generateTile(tilesPerRow, j);
+            let colJ = generateTile(tilesPerRow, j);
             rowI.appendChild(colJ);
         }
         boardDiv.appendChild(rowI);
     }
 }
 
-let tilesPerRow = prompt("Please enter a size for number of squares on each side of the grid:");
-createGrid(tilesPerRow);
+function hookUpListeners() {
+    blackButton.addEventListener("click", () => coloringValue = COLORING_MODE.BLACK);
+    rainbowButton.addEventListener("click", () => coloringValue = COLORING_MODE.RAINBOW);
+    eraserButton.addEventListener("click", () => coloringValue = COLORING_MODE.ERASER);
+    // Avoid needing to click every pixel. Makes it so clicking and dragging continues to draw
+    boardDiv.addEventListener("mousedown", () => (mousePressed = true));
+    boardDiv.addEventListener("mouseup", () => (mousePressed = false));
+    boardDiv.addEventListener('mouseleave', () => mousePressed = false);
+}
+
+function setUpEtchASketch() {
+    let tilesPerRow = prompt("Please enter a size for number of squares on each side of the grid:");
+    createGrid(tilesPerRow);
+    hookUpListeners();
+}
+
+setUpEtchASketch();
